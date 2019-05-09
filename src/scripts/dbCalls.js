2a74a924359bf9685dd5938896aa5b4ace53e424
Joey Driscoll <driscoll.recording@gmail.com>
2019-05-09 09:32:28 -0500

    initial jd commit

---
+++ src/scripts/dbCalls.js
@@ -0,0 +1,35 @@
+const src = "http://localhost:8088"
+const API = {
+    getCall: (resource) => {
+        return fetch(`${src}/${resource}`)
+            .then(result => result.json())
+
+    },
+    deleteCall: (resource, id) => {
+        return fetch(`${src}/${resource}/${id}`, {
+            method: "DELETE"
+        })
+    },
+    postCall: (resource, data) => {
+        return fetch(`${src}/${resource}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+    },
+    putCall: (resource, id, data) => {
+        return fetch(`${src}/${resource}/${id}`, {
+            method: "PUT",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+    }
+}
+
+export default API
