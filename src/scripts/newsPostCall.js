c846ce14d87ec37dec1d6d74616732eff47853e4
jksiggy <47400504+jksiggy@users.noreply.github.com>
2019-05-13 20:38:33 -0500

    timestamp

--- src/scripts/newsPostCall.js
+++ src/scripts/newsPostCall.js
@@ -1,14 +1,18 @@
 import API from "./dbCalls"
 
-function addNews(){
+function addNews() {
     const newsTitle = document.getElementById("news_title")
     const newsSynopsis = document.getElementById("news_synopsis")
     const newsUrl = document.getElementById("news_url")
-    console.log("newsTitle", newsTitle);
+    const stampDate = Date();
+    const newsTime = (stampDate);
+    console.log(newsTime)
     const newsObj = {
         title: newsTitle.value,
         synopsis: newsSynopsis.value,
-        url: newsUrl.value
+        url: newsUrl.value,
+        time: newsTime
+
     }
     console.log("newsObj", newsObj);
     API.postCall("news", newsObj)
